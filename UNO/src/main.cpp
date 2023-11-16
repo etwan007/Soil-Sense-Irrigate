81b60fc40b93cf82a041f5dcee1416e370a91042
Ethan Fuller <etwan007@gmail.com>
2023-11-15 19:11:10 -0500

    Uno=RX , implemented SD Logging

--- UNO/src/main.cpp
+++ UNO/src/main.cpp
@@ -1,17 +1,30 @@
+//Libraries
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 #include <LoRa.h>
 #include <SPI.h>
+#include <SD.h>
 
+//SD Card
+char sdPin = 10;
+File myFile;
+
+//LED Variables
 int ledG = 2;
 int ledY = 3;
 int ledR = 4;
-int moistureValue = 0;
 
+//LoRa Communication
 #define SS_PIN 10
 #define RST_PIN 9
 #define DI0_PIN 2
 #define frequency 433E6
+
+//Moisture
+int moistureValue = 0;
+
+//Function Prototypes
+void dataLog();
 
 
 void setup() {
@@ -28,13 +41,19 @@ void setup() {
   }
   Serial.println("LoRa initialization successful!");
 
+  //SD card
+  while (!SD.begin(sdPin)) {
+    Serial.println("Not Initialized");    //Debugging
+    delay(1000);
+  }
+  Serial.println("SD initialized");   //Debugging
+
   pinMode(ledG, OUTPUT);  // Set the LED pin as an output
   pinMode(ledY, OUTPUT);  // Set the LED pin as an output
   pinMode(ledR, OUTPUT);  // Set the LED pin as an output
 }
 
 void loop() {
-
  int packetSize = LoRa.parsePacket();
 
   if (packetSize) {
@@ -49,6 +68,8 @@ void loop() {
 
     Serial.println();
   }
+
+  dataLog(moistureValue);
 
     if (moistureValue > 60) //Moisture is ok
     {
@@ -72,3 +93,19 @@ void loop() {
     delay(1000);
 }
 
+void dataLog(int moistureValue) {
+  // Open the file in write mode
+  myFile = SD.open("data/moisture.txt", FILE_WRITE);
+
+  // Check if the file opened successfully
+  if (myFile) {
+    // Write the moisture value to the file
+    myFile.println(String(moistureValue) + " ");
+    
+    // Close the file
+    myFile.close();
+  } else {
+    // If the file couldn't be opened, print an error message
+    Serial.println("Error opening file for writing");   //DeBugging
+  }
+}
