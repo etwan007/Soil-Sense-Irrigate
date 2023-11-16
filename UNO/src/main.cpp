634bfd2c2dc6f24798559f88b7eb650a41bd52c1
Ethan Fuller <etwan007@gmail.com>
2023-11-16 01:26:52 -0500

    With lmic library, kinda bad and large memory

--- UNO/src/main.cpp
+++ UNO/src/main.cpp
@@ -1,30 +1,50 @@
 //Libraries
 #include <Arduino.h>
-#include <SoftwareSerial.h>
 #include <LoRa.h>
 #include <SPI.h>
 #include <SD.h>
+#include <lmic.h>
+#include <hal/hal.h>
 
 //SD Card
 char sdPin = 10;
 File myFile;
 
 //LED Variables
-int ledG = 2;
-int ledY = 3;
-int ledR = 4;
+// int ledG = 2;
+// int ledY = 3;
+// int ledR = 4;
 
 //LoRa Communication
 #define SS_PIN 10
 #define RST_PIN 9
 #define DI0_PIN 2
+const lmic_pinmap lmic_pins = {
+    .nss = 10,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = 9,
+    .dio = {2, 6, LMIC_UNUSED_PIN},
+};
 #define frequency 433E6
+static const u1_t DEVEUI[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static const u1_t APPEUI[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static const u1_t APPKEY[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8); }
+void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8); }
+void os_getDevKey (u1_t* buf) { memcpy_P(buf, APPKEY, 16); }
+
+static osjob_t sendjob;
+// User-defined data to send
+static const char* payload = "Hello, TTN!";
 
 //Moisture
 int moistureValue = 0;
 
 //Function Prototypes
-void dataLog();
+void dataLog(int);
+void do_send(osjob_t* j);
+void onEvent (ev_t ev);
 
 
 void setup() {
@@ -41,16 +61,25 @@ void setup() {
   }
   Serial.println("LoRa initialization successful!");
 
+  // LMIC init
+  os_init();
+
+  // Reset the MAC state. Session and pending data transfers will be discarded.
+  LMIC_reset();
+
+  // Start job (sending automatically starts OTAA too)
+  do_send(&sendjob);
+
   //SD card
   while (!SD.begin(sdPin)) {
     Serial.println("Not Initialized");    //Debugging
     delay(1000);
   }
-  Serial.println("SD initialized");   //Debugging
+  //Serial.println("SD initialized");   //Debugging
 
-  pinMode(ledG, OUTPUT);  // Set the LED pin as an output
-  pinMode(ledY, OUTPUT);  // Set the LED pin as an output
-  pinMode(ledR, OUTPUT);  // Set the LED pin as an output
+  // pinMode(ledG, OUTPUT);  // Set the LED pin as an output
+  // pinMode(ledY, OUTPUT);  // Set the LED pin as an output
+  // pinMode(ledR, OUTPUT);  // Set the LED pin as an output
 }
 
 void loop() {
@@ -71,24 +100,27 @@ void loop() {
 
   dataLog(moistureValue);
 
-    if (moistureValue > 60) //Moisture is ok
-    {
-      digitalWrite(ledG, HIGH);
-      digitalWrite(ledY, LOW);
-      digitalWrite(ledR, LOW);
-    }
-    else if (moistureValue > 30) //MOisture is close
-     {
-      digitalWrite(ledG, LOW);
-       digitalWrite(ledY, HIGH);
-      digitalWrite(ledR, LOW);
-     }
-    else //Needs sprinklers
-     {
-      digitalWrite(ledG, LOW);
-      digitalWrite(ledY, LOW);
-      digitalWrite(ledR, HIGH);
-    }
+  //Send the Data
+  os_runloop_once();
+
+    // if (moistureValue > 60) //Moisture is ok
+    // {
+    //   digitalWrite(ledG, HIGH);
+    //   digitalWrite(ledY, LOW);
+    //   digitalWrite(ledR, LOW);
+    // }
+    // else if (moistureValue > 30) //MOisture is close
+    //  {
+    //   digitalWrite(ledG, LOW);
+    //    digitalWrite(ledY, HIGH);
+    //   digitalWrite(ledR, LOW);
+    //  }
+    // else //Needs sprinklers
+    //  {
+    //   digitalWrite(ledG, LOW);
+    //   digitalWrite(ledY, LOW);
+    //   digitalWrite(ledR, HIGH);
+    // }
 
     delay(1000);
 }
@@ -107,5 +139,36 @@ void dataLog(int moistureValue) {
   } else {
     // If the file couldn't be opened, print an error message
     Serial.println("Error opening file for writing");   //DeBugging
+  }
+}
+
+
+void do_send(osjob_t* j) {
+  // Check if there is not a current TX/RX job running
+  if (LMIC.opmode & OP_TXRXPEND) {
+    Serial.println("OP_TXRXPEND, not sending");
+  } else {
+    // Prepare upstream data transmission at the next possible time.
+    LMIC_setTxData2(1, (uint8_t*)payload, strlen(payload), 0);
+    Serial.println("Packet queued");
+  }
+}
+
+void onEvent (ev_t ev) {
+  switch(ev) {
+    case EV_TXCOMPLETE:
+      Serial.println("EV_TXCOMPLETE (includes waiting for RX windows)");
+      if (LMIC.txrxFlags & TXRX_ACK)
+        Serial.println("Received ack");
+      if (LMIC.dataLen) {
+        Serial.println("Received ");
+        Serial.println(LMIC.dataLen);
+        Serial.println(" bytes of payload");
+      }
+      // Schedule next transmission
+      os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(5), do_send);
+      break;
+    default:
+      break;
   }
 }
